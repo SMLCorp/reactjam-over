a590200cf86fa172bbdbead1915139c87d4d98d7
Sonny <sonny.miel@gmail.com>
2023-10-23 23:59:27 +0200

    chore: add rendering library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Container, Sprite, Stage, Text } from "@pixi/react";
 import "./style/App.css";
 
 function App() {
   return (
-    <>
-    </>
+    <Stage>
+      <Sprite
+        image="https://pixijs.io/pixi-react/img/bunny.png"
+        x={400}
+        y={270}
+        anchor={{ x: 0.5, y: 0.5 }}
+      />
+
+      <Container x={400} y={330}>
+        <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} />
+      </Container>
+    </Stage>
   )
 }
 
