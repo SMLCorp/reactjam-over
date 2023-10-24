cea2ffd4347e49d4e93f596d57aa33fa2e6707e0
Sonny <sonny.miel@gmail.com>
2023-10-25 00:16:13 +0200

    chore: add button to test actions in front

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import { Container, Sprite, Stage, Text } from "@pixi/react";
+import { Sprite, Stage } from "@pixi/react";
+import { useWindowSize } from "react-use";
 import "./style/App.css";
+import type { ItemId } from "./type/game-state/item.type";
+import type { SpawnerId } from "./type/game-state/spawner.type";
+import type { TrashId } from "./type/game-state/trash.type";
 
 function App() {
-  return (
-    <Stage>
-      <Sprite
-        image="https://pixijs.io/pixi-react/img/bunny.png"
-        x={400}
-        y={270}
-        anchor={{ x: 0.5, y: 0.5 }}
-      />
+  const { width, height } = useWindowSize();
 
-      <Container x={400} y={330}>
-        <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} />
-      </Container>
-    </Stage>
+  return (
+    <>
+      <Stage width={width / 2} height={height / 2}>
+        <Sprite
+          x={width / 2}
+          y={height / 2}
+          anchor={0.5}
+          image="https://pixijs.com/assets/bunny.png"
+        />
+      </Stage>
+      <div style={{ position: "absolute", top: height / 2 + 20, left: 20 }}>
+        <button onClick={(): void => Rune.actions.dropItem("abc" as ItemId)}>
+          Drop Item
+        </button>
+        <button onClick={(): void => Rune.actions.spawnItem("abc" as SpawnerId)}>
+          Spawn Item
+        </button>
+        <button onClick={(): void => Rune.actions.deleteItem("abc" as TrashId)}>
+          Delete Item
+        </button>
+
+
+      </div>
+    </>
   );
 }
 
