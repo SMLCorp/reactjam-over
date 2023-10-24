0db1e56977da88c8e9e4ef75a2e137e7fb50cb9e
Sonny <sonny.miel@gmail.com>
2023-10-24 11:11:39 +0200

    chore: add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,21 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
-    "plugin:rune/recommended"
+    "plugin:rune/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs", "**/*.typegen.ts"],
   parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": [
@@ -17,4 +25,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
